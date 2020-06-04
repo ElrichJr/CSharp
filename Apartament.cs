b09829c7627a65dd496e1bff0f069e07cf6d1e1a
ElrichJr <61782822+ElrichJr@users.noreply.github.com>
2020-06-04 17:09:06 +0300

    Add files via upload

---
+++ Apartament.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    struct Apartament
+    {
+        public Kitchen kitchen;
+        public Bedroom[] bedroom;
+        public Room[] room;
+        public Apartament(int bedroomNumber, int roomNumber)
+        {
+            kitchen = new Kitchen();
+            bedroom = new Bedroom[bedroomNumber];
+            room = new Room[roomNumber];
+        }
+    }
+}
